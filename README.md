acea3e9dfd9d2f5efef75ed62a263498159f9ad3
Eric Denovellis <edeno@bu.edu>
2021-03-02 16:12:37 -0800

    Update package name

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# <repository_name>
+# <mc_decoding>
 Repository description goes here.
 
 
 ## Installation
-1. git clone <repository>
+1. git clone https://github.com/edeno/mc_decoding.git
 2. Add dependencies to setup.py and environment.yml
 3. Create conda environment:
 ```conda env create -f environment.yml```
 4. Make package editable:
 ```python setup.py develop```
-
-
--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@
 from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = ['numpy', 'numba', 'scipy', 'scikit-learn', 'matplotlib',
-                    'xarray', 'pandas', 'dask', 'tqdm', 'statsmodels', 'patsy']
+                    'xarray', 'pandas', 'seaborn',
+                    'loren_frank_data_processing',
+                    'ripple_detection',
+                    'replay_trajectory_classification',
+                    'trajectory_analysis_tools']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
-    name='analysis_template',
+    name='mc_decoding',
     version='0.1.0.dev0',
     license='MIT',
     description=(''),
