d4065d2374d982fec967afaf780bada3b6dab6fa
Michael Coulter <michael.e.coulter@gmail.com>
2021-07-07 18:34:50 -0700

    updated parameters script

--- src/parameters.py
+++ src/parameters.py
@@ -15,6 +15,15 @@ FIGURE_DIR = join(ROOT_DIR, 'figures')
 ANIMALS = {
     'david905': Animal(directory='/stelmo/mcoulter/david905/filterframework',
                        short_name='david905'),
+
+    #'CH105': Animal(directory='/stelmo/mcoulter/CH105/filterframework',short_name='CH105'),
+
+    'CH105': Animal(directory='/stelmo/david/Scn2a/coh2/CH105/AutoW/filterframework',short_name='CH105'),
+    'CH112': Animal(directory='/stelmo/david/Scn2a/coh2/CH112/AutoW/filterframework',short_name='CH112'),
+    'CH109': Animal(directory='/stelmo/david/Scn2a/coh2/CH109/AutoW/filterframework',short_name='CH109'),
+    'CH101': Animal(directory='/stelmo/david/Scn2a/coh2/CH101/AutoW/filterframework',short_name='CH101'),
+
+    'CH6': Animal(directory='/stelmo/mcoulter/CH6/filterframework',short_name='CH6'),
 }
 
 EDGE_ORDER = np.asarray(
@@ -24,18 +33,19 @@ EDGE_ORDER = np.asarray(
         (15, 13),
         (13, 12),
         (14, 13),
-        (12, 5),
-        (5, 4),
-        (5, 3),
-        (3, 2),
-        (3, 1),
-        (1, 0),
-        (12, 7),
-        (7, 6),
-        (7, 9),
-        (9, 8),
-        (9, 11),
-        (11, 10),
+        (1,0),
+        (1,3),
+        (3,2),
+        (3,5),
+        (5,4),
+        (5,12),
+        (12,7),
+        (7,6),
+        (7,9),
+        (9,8),
+        (9,11),
+        (11,10),
+
     ]
 )
 
